528de5b59dc38cb59852cb29cf2a3dbc0b201779
shubhivermax <shubhiverma@usf.edu>
2025-09-27 05:21:44 -0400

    side navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MantineProvider>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
